e93bb4859811e711cedbcdd9435b8d1956b87d50
Megan <66979053+Shecodes1@users.noreply.github.com>
2023-11-16 20:48:21 -0500

    YoutubeAnalysis.sql

--- YoutubeAnalysis.sql
+++ YoutubeAnalysis.sql
@@ -1,13 +1,20 @@
-CREATE TABLE YOUTUBERINFO(
-rank NUMBER,
-rank_in_country NUMBER,
-youtuber VARCHAR2 (255),
-category VARCHAR2 (255),
-uploads NUMBER, 
-Country VARCHAR2 (255),
-channel_type VARCHAR(255),
-created_date DATE,
-PRIMARY KEY (YOUTUBER)
+CREATE TABLE YOUTUBERINFO (
+    rank NUMBER,
+    rank_in_country NUMBER,
+    youtuber VARCHAR2(255),
+    category VARCHAR2(255),
+    uploads NUMBER, 
+    Country VARCHAR2(255),
+    channel_type VARCHAR(255),
+    created_date DATE,
+    PRIMARY KEY (youtuber)
+);
+
+CREATE TABLE COUNTRYSTATS (
+    countryID NUMBER, 
+    Country VARCHAR2(255),
+    population NUMBER,
+    PRIMARY KEY (Country)
 );
 
 CREATE TABLE channel_stats (
@@ -18,13 +25,8 @@ CREATE TABLE channel_stats (
     VIDEO_VIEWS NUMBER,
     LOWEST_YEARLY_EARNINGS NUMBER,
     HIGHEST_YEARLY_EARNINGS NUMBER,
-    FOREIGN KEY (youtuber) REFERENCES YOUTUBERINFO(youtuber)
-);
-
-CREATE TABLE COUNTRYSTATS(
-countryID NUMBER, 
-Country VARCHAR2 (255),
-population NUMBER,
-PRIMARY KEY (countryID)
+    Country VARCHAR2(255),
+    FOREIGN KEY (YOUTUBER) REFERENCES YOUTUBERINFO(youtuber),
+    FOREIGN KEY (Country) REFERENCES COUNTRYSTATS(Country)
 );
 
